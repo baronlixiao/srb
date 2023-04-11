18d27dd7ee8e43c122b140114a730bb91849198a
baronlixiao <lixiaobaron@163.com>
2023-04-12 00:51:38 +0800

    oss，sms

--- service-base/src/main/java/com/atguigu/srb/base/config/Swagger2Config.java
+++ service-base/src/main/java/com/atguigu/srb/base/config/Swagger2Config.java
@@ -17,24 +17,41 @@ public class Swagger2Config {
 
     @Bean
     public Docket adminApiConfig(){
-
         return new Docket(DocumentationType.SWAGGER_2)
                 .groupName("adminApi")
                 .apiInfo(adminApiInfo())
                 .select()
-                //只显示admin路径下的页面
                 .paths(Predicates.and(PathSelectors.regex("/admin/.*")))
                 .build();
-
     }
 
-    private ApiInfo adminApiInfo(){
 
+    private ApiInfo adminApiInfo(){
         return new ApiInfoBuilder()
-                .title("尚融宝后台管理系统-API文档")
-                .description("本文档描述了尚融宝后台管理系统接口")
-                .version("1.0")
-                .contact(new Contact("baron", "http://baidu.com", "baronlixiao@163.com"))
+                .title("尚融宝后台管理系统API文档")
+                .description("本文档描述了尚融宝后台管理系统的各个模块的接口的调用方式")
+                .version("1.6")
+                .contact(new Contact("Helen", "http://atguigu.com", "admin@atguigu.com"))
+                .build();
+    }
+
+
+    @Bean
+    public Docket webApiConfig(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("webApi")
+                .apiInfo(webApiInfo())
+                .select()
+                .paths(Predicates.and(PathSelectors.regex("/api/.*")))
+                .build();
+    }
+
+    private ApiInfo webApiInfo(){
+        return new ApiInfoBuilder()
+                .title("尚融宝网站API文档")
+                .description("本文档描述了尚融宝网站各个模块的接口的调用方式")
+                .version("1.6")
+                .contact(new Contact("Helen", "http://atguigu.com", "admin@atguigu.com"))
                 .build();
     }
 }
