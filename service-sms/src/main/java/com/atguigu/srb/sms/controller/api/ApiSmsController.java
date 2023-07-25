717cd0e1dd1cc38fe6cdb57f342db8867c7135db
baronlixiao <lixiaobaron@163.com>
2023-07-25 23:50:45 +0800

    提交测试5

--- service-sms/src/main/java/com/atguigu/srb/sms/controller/api/ApiSmsController.java
+++ service-sms/src/main/java/com/atguigu/srb/sms/controller/api/ApiSmsController.java
@@ -26,7 +26,7 @@ import java.util.concurrent.TimeUnit;
 public class ApiSmsController {
 
     @Resource
-    private SmsService smsServicesss;
+    private SmsService smsService;
 
     @Resource
     private RedisTemplate redisTemplate;
